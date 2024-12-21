4f9500c88bb35bcbdfe04f155184295a565a0a99
Andrey Ovchinnikov <andpey.ovchinnikov@yandex.ru>
2024-12-21 15:39:42 +0300

    добавил импорт слайса пользователя в RootReducer

--- src/services/RootReducer.tsx
+++ src/services/RootReducer.tsx
@@ -3,12 +3,14 @@ import burgerConstructorSlice from './slices/BurgerConstructorSlice';
 import feedInfoSlice from './slices/FeedInfoSlice';
 import orderSlice from './slices/OrderSlice';
 import ingredientsSlice from './slices/IngredientsSlice';
+import userSlice from './slices/UserSlice';
 
 const rootReducer = combineReducers({
   burgerConstructor: burgerConstructorSlice,
   feedInfo: feedInfoSlice,
   ingredients: ingredientsSlice,
-  order: orderSlice
+  order: orderSlice,
+  user: userSlice
 });
 
 export default rootReducer;
