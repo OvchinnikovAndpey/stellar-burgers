0e03a8c489ff26eba3c2c5f34be9e1a68b45665d
Andrey Ovchinnikov <andpey.ovchinnikov@yandex.ru>
2025-01-08 17:24:23 +0300

    create cypress tets

--- src/components/ingredients-category/ingredients-category.tsx
+++ src/components/ingredients-category/ingredients-category.tsx
@@ -7,7 +7,7 @@ import { useSelector } from '../../services/store';
 export const IngredientsCategory = forwardRef<
   HTMLUListElement,
   TIngredientsCategoryProps
->(({ title, titleRef, ingredients }, ref) => {
+>(({ title, titleRef, ingredients, ...rest }, ref) => {
   // Получаем состояние конструктора из стора
   const burgerConstructor = useSelector((state) => state.burgerConstructor);
 
@@ -29,6 +29,7 @@ export const IngredientsCategory = forwardRef<
       ingredients={ingredients}
       ingredientsCounters={ingredientsCounters}
       ref={ref}
+      {...rest}
     />
   );
 });
